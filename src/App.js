04c22a0a4062ab3606ac76c1309355db3c2f052d
kyuhong <104212768+KorOnechild@users.noreply.github.com>
2023-04-03 15:37:21 +0900

    declare dummyData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-import { data } from "./src/data/data.json";
-import { creations } from "./src/data/creations.json";
+import contents from "./data/data.json";
+import creations from "./data/creations.json";
 
 function App() {
+  const dummyData = {
+    contents: contents,
+    creations: creations
+  };
+
+  console.log(dummyData);
+
   return (
     <div className="App">
       Hi
