8dcdd73d362320a8f181e257cfaafdc7b8d8241a
kyuhong <104212768+KorOnechild@users.noreply.github.com>
2023-04-03 15:00:22 +0900

    init data in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { data } from "./src/data/data.json";
+import { creations } from "./src/data/creations.json";
+
 function App() {
   return (
     <div className="App">
